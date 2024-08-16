7deff427b3143c5fb73be9853f1d43af8672e9dc
Pablocorrea49 <167103781+Pablocorrea49@users.noreply.github.com>
2024-08-16 10:44:43 -0300

    Create_Convertir_una_cadena_en_una_matriz.js

---
+++ JavaScript/8 kyū/Convertir_una_cadena_en_una_matriz.js
@@ -0,0 +1,11 @@
+Escriba una función para dividir una cadena y convertirla en una matriz de palabras.
+
+  https://www.codewars.com/kata/57e76bc428d6fbc2d500036d/solutions/javascript
+
+  function stringToArray(string){
+
+  return string.split(' ')
+	//let new_string = string.split(' ')
+  //return new_string
+
+}
