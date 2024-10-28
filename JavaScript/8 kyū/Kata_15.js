db26990cd7542153a30fca2b21355da1d0d6e435
Pablocorrea49 <167103781+Pablocorrea49@users.noreply.github.com>
2024-10-28 14:30:49 -0300

    valor booleano

---
+++ JavaScript/8 kyū/Kata_15.js
@@ -0,0 +1,8 @@
+Titulo: Complete el método que toma un valor booleano y devuelve una "Yes"cadena para true, o una "No"cadena para false
+
+https://www.codewars.com/kata/53369039d7ab3ac506000467/solutions/javascript
+
+
+function boolToWord( bool ){
+    return bool ? 'Yes' : 'No'
+  }
