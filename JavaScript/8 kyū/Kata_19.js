5341091e73409b3e1a2225e5643cfa8d031ff95f
Pablocorrea49 <167103781+Pablocorrea49@users.noreply.github.com>
2024-10-29 14:47:45 -0300

    Corregir script

---
+++ JavaScript/8 kyū/Kata_19.js
@@ -0,0 +1,7 @@
+Titulo: Este código no se ejecuta correctamente. Intenta averiguar por qué.
+
+https://www.codewars.com/kata/50654ddff44f800200000004/solutions/javascript
+
+function multiply(a, b){
+  return a * b
+}
