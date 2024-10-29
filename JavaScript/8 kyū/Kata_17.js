58bff455956b2337b998bdb8d1e3f843adb029ad
Pablocorrea49 <167103781+Pablocorrea49@users.noreply.github.com>
2024-10-28 23:10:01 -0300

    Eliminar espacios en blanco

---
+++ JavaScript/8 kyū/Kata_17.js
@@ -0,0 +1,7 @@
+Titulo: Escriba una función que elimine los espacios de la cadena y luego devuelva la cadena resultante.
+
+https://www.codewars.com/kata/57eae20f5500ad98e50002c5/solutions/javascript
+
+function noSpace(x){
+    return x.replaceAll(' ','');
+}
