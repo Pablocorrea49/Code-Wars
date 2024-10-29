360a5c692a2f706993e6f669f55794d7639e10ce
Pablocorrea49 <167103781+Pablocorrea49@users.noreply.github.com>
2024-10-29 14:44:09 -0300

    cadena y convertirla en una matriz de palabras

---
+++ JavaScript/8 kyū/Kata_18.js
@@ -0,0 +1,9 @@
+Titulo: Escriba una función para dividir una cadena y convertirla en una matriz de palabras.
+
+https://www.codewars.com/kata/57e76bc428d6fbc2d500036d/solutions/javascript
+
+function stringToArray(string){
+  return string.split(' ');
+}
+
+console.log(stringToArray("Robin Singh"))
